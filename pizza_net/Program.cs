5a727dc0ae46478d8c3f18863f4260e3ce84a99c
Souleman <soulte92@gmail.Com>
2023-07-21 23:10:12 +0200

    Add relative paths

--- pizza_net/Program.cs
+++ pizza_net/Program.cs
@@ -1,19 +1,20 @@
-﻿using System.Text;
-
-namespace pizza_net
+﻿namespace pizza_net
 {
     public class Program
     {
         public static void Main()
         {
-            // string filePath = "";
-            // string filePath = "/Users/soulte92/Documents/SCHOOL/ESGI-4AL/S2/DESIGN PATTERN/PROJECTS/pizza-net/pizza_net/testPizzaMenu.txt";
-            // string filePath = "/Users/soulte92/Documents/SCHOOL/ESGI-4AL/S2/DESIGN PATTERN/PROJECTS/pizza-net/pizza_net/testPizzaMenu.json";
-            string filePath = "/Users/soulte92/Documents/SCHOOL/ESGI-4AL/S2/DESIGN PATTERN/PROJECTS/pizza-net/pizza_net/testPizzaMenu.xml";
+            // string relativeFilePath = @"";
+            // string relativeFilePath = @"..testPizzaMenu.txt";
+            // string relativeFilePath = @"..testPizzaMenu.json";
+            string relativeFilePath = @"..testPizzaMenu.xml";
 
+            // Get Absolute path
+            string baseDirectory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(baseDirectory, relativeFilePath).Replace("bin/Debug/net7.0/..", "");
+            
             var pizzaNet = new PizzaNet();
             pizzaNet.launchPizzeria(filePath);
         }
     }
 }
-
